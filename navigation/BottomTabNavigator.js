c9a3147f227c16286efe03ce3370fd22c6c28f76
zd <mikezhangding@hotmail.com>
2020-05-28 17:30:23 +0800

    sdfsdfaaaaaaaa

--- navigation/BottomTabNavigator.js
+++ navigation/BottomTabNavigator.js
@@ -1,52 +1,63 @@
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import * as React from 'react';
 
 import TabBarIcon from '../components/TabBarIcon';
 import HomeScreen from '../screens/HomeScreen';
+import HomeScreenNew from '../screens/HomeScreenNew';
 import LinksScreen from '../screens/LinksScreen';
 
 const BottomTab = createBottomTabNavigator();
 const INITIAL_ROUTE_NAME = 'Home';
 
-export default function BottomTabNavigator({ navigation, route }) {
-  // Set the header title on the parent stack navigator depending on the
-  // currently active tab. Learn more in the documentation:
-  // https://reactnavigation.org/docs/en/screen-options-resolution.html https://reactnavigation.org/docs/bottom-tab-navigator#!
-  navigation.setOptions({ headerTitle: getHeaderTitle(route) });
+export default function BottomTabNavigator({navigation, route}) {
+    // Set the header title on the parent stack navigator depending on the
+    // currently active tab. Learn more in the documentation:
+    // https://reactnavigation.org/docs/en/screen-options-resolution.html https://reactnavigation.org/docs/bottom-tab-navigator#!
+    navigation.setOptions({headerTitle: getHeaderTitle(route)});
 
-  return (
+    return (
         <BottomTab.Navigator
-        initialRouteName={INITIAL_ROUTE_NAME}
-        tabBarOptions={{
-        activeTintColor: '#1ea2e9',
-    }}>
-      <BottomTab.Screen
-        name="Home"
-        component={HomeScreen}
-        options={{
-          title: 'Get Started',
-          tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-book" />,
-        }}
-      />
-      <BottomTab.Screen
-        name="Links"
-        component={LinksScreen}
-        options={{
-          title: 'Resources',
-          tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-person" />,
-        }}
-      />
-    </BottomTab.Navigator>
-  );
+            initialRouteName={INITIAL_ROUTE_NAME}
+            tabBarOptions={{
+                activeTintColor: '#1ea2e9',
+            }}>
+            <BottomTab.Screen
+                name="Home"
+                component={HomeScreenNew}
+                options={{
+                    title: 'Home',
+                    tabBarIcon: ({focused}) => <TabBarIcon focused={focused} name="md-book"/>,
+                }}
+            />
+            <BottomTab.Screen
+                name="HomeNew"
+                component={HomeScreen}
+                options={{
+                    title: 'Home2',
+                    tabBarIcon: ({focused}) => <TabBarIcon focused={focused} name="md-book"/>,
+                }}
+            />
+            <BottomTab.Screen
+                name="Links"
+                component={LinksScreen}
+                options={{
+                    title: 'Resources',
+                    tabBarIcon: ({focused}) => <TabBarIcon focused={focused} name="md-person"/>,
+                }}
+            />
+        </BottomTab.Navigator>
+    );
 }
 
 function getHeaderTitle(route) {
-  const routeName = route.state?.routes[route.state.index]?.name ?? INITIAL_ROUTE_NAME;
+    const routeName = route.state?.routes[route.state.index]?.name ?? INITIAL_ROUTE_NAME;
 
-  switch (routeName) {
-    case 'Home':
-      return 'How to get started';
-    case 'Links':
-      return 'Links to learn more';
-  }
+    switch (routeName) {
+        case 'Home':
+            return 'English Ability';
+        case 'HomeNew':
+            return 'How to get started';
+        case 'Links':
+            return 'Links to learn more';
+    }
 }
---
+++ screens/HomeScreenNew.js
@@ -0,0 +1,59 @@
+import * as WebBrowser from 'expo-web-browser';
+// import * as React from 'react';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {Image, Platform, StyleSheet, Text, TouchableOpacity, View, Button} from 'react-native';
+import {ScrollView} from 'react-native-gesture-handler';
+
+import {MonoText} from '../components/StyledText';
+
+import {styles} from '../style/homeScreenNewStyle';
+
+import HomeDetail from "./HomeDetail";
+
+import {requestData} from '../redux/actions/userAction';
+
+
+class HomeScreenNew extends Component {
+
+    render() {
+
+        return <ScrollView style={styles.container}>
+                 {/* header title */}
+                 <View style={styles.headView}>
+                     <View>
+                        <Text style={styles.headerTitle}>Today Reading!</Text>
+                     </View>
+                     <View>
+                         <Text style={styles}>Read more, Learn more.</Text>
+                     </View>
+                 </View>
+
+                 {/* first article image */}
+                 <View style={styles.firstArticleImg}>
+                      <Text>9999</Text>
+                 </View>
+
+               </ScrollView>
+
+    }
+}
+
+
+function mapStateToProps(state) {
+    return {
+        flag: state.userReducer.flag,
+        user: state.userReducer.user,
+        jokerVideo: state.userReducer.jokerVideo
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        updateData: function () {
+            dispatch(requestData());
+        }
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomeScreenNew);
