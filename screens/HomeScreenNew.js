22cdb3bfbedfdc4556d669b84220b6f25fb482f4
zd <mikezhangding@hotmail.com>
2020-05-29 14:04:16 +0800

    CardArticle.js

--- screens/HomeScreenNew.js
+++ screens/HomeScreenNew.js
@@ -12,9 +12,12 @@ import {
     Button,
     Alert,
     ImageBackground,
+    FlatList,
+    SectionList,
     StatusBar, Dimensions
 } from 'react-native';
 import MyButton from '../components/MyButton'
+import CardArticle from '../components/CardArticle'
 import {ScrollView} from 'react-native-gesture-handler';
 
 import {MonoText} from '../components/StyledText';
@@ -23,14 +26,12 @@ import {styles} from '../style/homeScreenNewStyle';
 import Constants from 'expo-constants';   // 常量
 
 
-
 import HomeDetail from "./HomeDetail";
 
 import {requestData} from '../redux/actions/userAction';
 import {ImageBackground as WebImageBackground} from "react-native-web";
 // import {BoxShadow} from 'react-native-shadow'
 
-const image = {uri: "https://reactjs.org/logo-og.png"};
 
 class HomeScreenNew extends Component {
 
@@ -45,50 +46,9 @@ class HomeScreenNew extends Component {
                     <Text style={styles.headerDes}>Read more, Learn more.</Text>
                 </View>
             </View>
-
-            {/* first article image */}
-                 <View style={styles.firstArticleImg}>
-                    {Platform.OS === 'web' ? (<WebImageBackground source={image} style={styles.insideImg}>
-                        <Text style={styles.insideTitleFirst}>No.209 | Hot News</Text>
-                        <Text style={styles.insideMainTitle}>Inside the home land ok now hello world!</Text>
-                        <View style={{
-                            flexDirection: "row",
-                            height: 100,
-                            padding: 20
-                        }}>
-                            <View style={{backgroundColor: "blue", flex: 0.4}}></View>
-                            <View style={{backgroundColor: "red", flex: 0.4}}></View>
-                        </View>
-                    </WebImageBackground>) : (<ImageBackground source={image} style={styles.insideImg}>
-                        <Text style={styles.insideTitleFirst}>No.209 | Hot News</Text>
-                        <Text style={styles.insideMainTitle}>Inside the home land ok now hello world!</Text>
-                        <View style={{
-                            flexDirection: "row",
-                            justifyContent: 'space-between',
-                            height: 100,
-                            lineHeight: 100,
-                            position: 'relative',
-                            top: 260,
-                            left: -20,
-                            padding: 20
-                        }}>
-                            <View style={styles.cardTimeStyle}>
-                                <Text style={{color: 'white', }}>May.29th.2020</Text>
-                            </View>
-                            {/*<Button title="Start Read" color="white" onPress={() => Alert.alert('Right button pressed')} />*/}
-                            <MyButton
-                                text={'Read Now'}
-                                onPress={() => Alert.alert('Right button pressed')}
-                                bgColor={'green'}
-                                fColor={'white'}
-                                style={{borderRadius: 4}}
-                                size={20}
-                            />
-                        </View>
-                    </ImageBackground>)}
-                </View>
-
-        </ScrollView>)
+                <CardArticle/>
+        </ScrollView>
+       )
 
     }
 }
